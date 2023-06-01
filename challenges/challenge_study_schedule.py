9469d87649b80ef1f9ce77c657d54b40e1b08699
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-06-01 17:51:45 -0300

    desafio 1 - correção erros flake

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,10 @@
 from collections import Counter
 
+
 def study_schedule(permanence_period, target_time):
     rangeList = []
     for time_preriod in permanence_period:
-      rangeList.extend(range(time_preriod[0], time_preriod[1]+1))
+        rangeList.extend(range(time_preriod[0], time_preriod[1]+1))
     counter = Counter(rangeList)
     # print(counter[target_time])
     return counter[target_time]
