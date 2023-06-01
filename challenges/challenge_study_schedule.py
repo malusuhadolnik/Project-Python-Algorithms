117678e62bc82bf370d4787641972f3e9559f649
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-06-01 18:37:30 -0300

    desafio 1 - adição da exceção em um bloco de try/except

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,10 +2,15 @@ from collections import Counter
 
 
 def study_schedule(permanence_period, target_time):
+    if not permanence_period or not target_time:
+        return None
     rangeList = []
     for time_preriod in permanence_period:
-        rangeList.extend(range(time_preriod[0], time_preriod[1]+1))
+        try:
+            rangeList.extend(range(time_preriod[0], time_preriod[1]+1))
+        except TypeError:
+            return None
+
     counter = Counter(rangeList)
-    # print(counter[target_time])
     return counter[target_time]
-    raise NotImplementedError
+    # raise NotImplementedError
