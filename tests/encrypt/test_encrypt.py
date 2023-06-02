ae300b42beae562418f0a1aa03ecd5ffe22f326a
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-06-02 10:52:27 -0300

    desafio 2 - testes para a função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+# Referências bibliográficas:
+# https://stackoverflow.com/questions/23337471/how-to-properly-assert-that-an-exception-gets-raised-in-pytest
+# https://docs.pytest.org/en/7.1.x/reference/reference.html#pytest-raises
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    message = "buapau"  # iniciais dos emojis do readme
+    invalid_message = 3
+    invalid_key = "buapau"
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(invalid_message, 3)
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message(message, invalid_key)
+    assert encrypt_message(message, 9) == "uapaub"
+    assert encrypt_message(message, 3) == "aub_uap"
+    assert encrypt_message(message, 4) == "ua_paub"
