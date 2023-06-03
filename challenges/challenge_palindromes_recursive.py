b16f59ed704f01962770b739df2e1622a37ab7a8
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-06-03 13:49:09 -0300

    desafio 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    if low_index >= high_index:  # condição base
+        return True
+    if word[low_index] == word[high_index]:  # itera enquanto forem iguais,
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return False  # se lowindex diferente de highindex, então não é palíndromo
+
+
+# is_palindrome_recursive('ANA', 0, 2)
