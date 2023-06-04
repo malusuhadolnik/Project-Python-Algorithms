9802a1ca19a10e33e1a9d440aa6c135945e49503
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-06-04 09:44:01 -0300

    desafio 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,12 @@
+# Referência bibliográfica: 
+# https://www.geeksforgeeks.org/python-program-check-string-palindrome-not/
+
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+
+    backwards = ''.join(reversed(word))
+
+    if word == backwards:
+        return True
+    return False
