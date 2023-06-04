aa038ce6da89d2a7be71d005f4fcd337a005f57a
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-06-04 11:26:57 -0300

    desafio 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def find_duplicate(nums: list[int]):
+    is_array = isinstance(nums, list)
+    n = len(nums)
+
+    if not nums or is_array is False or n == 1:
+        return False
+
+    ordered = sorted(nums)
+    for i in range(0, n-1):
+        if ordered[i] == ordered[i + 1] and ordered[i] > 0:
+            return ordered[i]
+    return False
